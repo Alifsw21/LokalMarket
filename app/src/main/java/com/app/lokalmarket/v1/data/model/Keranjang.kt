1e8ac7ec6fd4f675be13531cb95e177dc63ec62d
Alif Satriya Wicaksono <alifsw2006@gmail.com>
2026-06-21 17:15:00 +0700

    refactor: hapus kolom gambar pada keranjang

--- app/src/main/java/com/app/lokalmarket/v1/data/model/Keranjang.kt
+++ app/src/main/java/com/app/lokalmarket/v1/data/model/Keranjang.kt
@@ -4,7 +4,6 @@ data class Keranjang (
     val id: Int = 0,
     val idProduk: Int,
     val namaProduk: String,
-    val gambarProduk: String,
     val hargaSatuan: Int,
     var jumlahBarang: Int
 )
