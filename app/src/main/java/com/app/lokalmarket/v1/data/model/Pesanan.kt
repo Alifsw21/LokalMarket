aa1de0de992b9815e7a5637e7d861713a3adb43d
Alif Satriya Wicaksono <alifsw2006@gmail.com>
2026-06-21 16:31:11 +0700

    refactor: ubah kolom alamatPengirim menjadi alamat

--- app/src/main/java/com/app/lokalmarket/v1/data/model/Pesanan.kt
+++ app/src/main/java/com/app/lokalmarket/v1/data/model/Pesanan.kt
@@ -5,6 +5,6 @@ data class Pesanan (
     val idPengguna: Int,
     val tanggalPesanan: String,
     val totalHarga: Int,
-    val alamatPengiriman: String,
+    val alamat: String,
     val statusPesanan: String
 )
